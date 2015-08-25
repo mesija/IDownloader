8b6059b5562204f417883f99776372250f1d0da5
s.rudavskii <s.rudavskii@magnetickone.com>
2015-08-25 12:49:44 +0300

    ver 3.2.6, add text in update log

--- core/update_log.php
+++ core/update_log.php
@@ -1,6 +1,9 @@
 <?php
 
 $UPDATE_INFO = array(
+  '3.2.6' => array(
+    'Small fix update process',
+  ),
   '3.2.5' => array(
     'Change check version method to version_compare()',
     'Add update log',
